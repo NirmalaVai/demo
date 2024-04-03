24b85286f0d0b561328339e0f26ee8b8c9e3202b
NirmalaVai <nimmicsegct@gmail.com>
2024-04-03 12:38:06 -0500

    firstCommit

---
+++ src/main/java/com/example/Sample.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Sample {
+
+    public void printSample(){
+        System.out.println("Hello World!");
+    }
+    public static void main(String args[]){
+
+        Sample s =  new Sample();
+        s.printSample();
+    }
+    
+}
+
